78aed367a2df85a3f9fe66ce130a561bdb4ce78f
Mr.N0BODY <51495170+N0BODY2@users.noreply.github.com>
2021-03-26 14:23:00 +0100

    Weight implement

--- sql.sql
+++ sql.sql
@@ -1,4 +1,12 @@
+-- Limits
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES
 	('id_card', 'ID CARD', -1),
 	('license_drive', 'DRIVING LICENSE', -1),
-	('license_weapon', 'WEAPON LICENSE', -1);
+	('license_weapon', 'WEAPON LICENSE', -1)
+;
+-- Weight
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('id_card', 'ID CARD', 1),
+	('license_drive', 'DRIVING LICENSE', 1),
+	('license_weapon', 'WEAPON LICENSE', 1)
+;		
